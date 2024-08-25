256c7d6cf2679f1895b5fd33580d2471e3073c36
Tamoghna35 <60691052+Tamoghna35@users.noreply.github.com>
2024-08-25 16:46:46 +0530

    write logic for utility functions

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,15 @@
-console.log(Hi);
+import express from "express"
+const app = express();
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(cookieParser())
+
+export {app}
